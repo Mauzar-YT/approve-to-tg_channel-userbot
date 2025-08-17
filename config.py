c842d44bd403e61963d7583f4841b601576b0323
Mauzar-YT <83205154+Mauzar-YT@users.noreply.github.com>
2025-08-17 20:22:42 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Telegram API credentials
 # Получите их на https://my.telegram.org/apps
-API_ID = 20512430  # Замените на ваш API_ID
-API_HASH = "80a65f8a496198608d8e68d837c4f798"  # Замените на ваш API_HASH
+API_ID = 3424234  # Замените на ваш API_ID
+API_HASH = "амавмамм"  # Замените на ваш API_HASH
